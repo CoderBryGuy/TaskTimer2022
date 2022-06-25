0724f9120a276a64850a9dfaed9561a16b1d7413
bryan <bryan.dov.bergman@gmail.com>
2022-06-25 20:52:38 +0300

    Updated Menu XML

--- app/src/main/java/com/example/tasktimer2022/MainActivity.java
+++ app/src/main/java/com/example/tasktimer2022/MainActivity.java
@@ -142,7 +142,7 @@ MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.mainmenu_settings) {
             return true;
         }
 
